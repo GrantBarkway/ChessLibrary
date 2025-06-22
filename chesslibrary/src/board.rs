068e1e3370517e09e4149b536e4fa86f338dae0e
Grant Barkway <grantbarkway@shaw.ca>
2025-06-21 20:14:52 -0700

    Need to work on a roadmap for this project.

---
+++ chesslibrary/src/board.rs
@@ -0,0 +1,43 @@
+pub struct Bitboard(pub u64);
+
+pub struct Board {
+    role: ByRole<Bitboard>,
+    colour: ByColour<Bitboard>,
+    occupied: Bitboard,
+}
+
+
+// changed up a bit from Shakmaty
+impl Board {
+    pub const fn new() -> Board {
+        Board {
+            role: ByRole {
+                pawn: Bitboard(0x00ff_0000_0000_ff00),
+                knight: Bitboard(0x4200_0000_0000_0042),
+                bishop: Bitboard(0x2400_0000_0000_0024),
+                rook: Bitboard(0x8100_0000_0000_0081),
+                queen: Bitboard(0x0800_0000_0000_0008),
+                king: Bitboard(0x1000_0000_0000_0010),
+            },
+            colour: ByColour {
+                black: Bitboard(0xffff_0000_0000_0000),
+                white: Bitboard(0xffff),
+            },
+            occupied: Bitboard(0xffff_0000_0000_ffff),
+        }
+    }
+}
+
+pub struct ByRole<T> {
+    pub pawn: T,
+    pub knight: T,
+    pub bishop: T,
+    pub rook: T,
+    pub queen: T,
+    pub king: T,
+}
+
+pub struct ByColour<T> {
+    pub white: T,
+    pub black: T,
+}
